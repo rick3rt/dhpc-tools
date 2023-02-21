0bf9746947bde68032e675de512ba3fd2412b8a1
R Waasdorp <r.waasdorp@tudelft.nl>
2023-02-21 03:37:07 +0100

    start with database

--- myslurm/slurmmanager.py
+++ myslurm/slurmmanager.py
@@ -4,6 +4,96 @@ import os
 import subprocess
 import pandas as pd
 from datetime import datetime
+from tabulate import tabulate
+
+
+class Database:
+    def __init__(self, columns, column_types):
+        self.cols = columns
+        self.data = []
+        self.column_types = column_types
+
+    def load_from_csv(self, filename):
+        with open(filename, 'rt', encoding='utf-8') as f:
+            line = f.readline()  # read header
+            lines = f.readlines()
+            for line in lines:
+                row = line.split(',')
+                for idx, c_type in enumerate(self.column_types):
+                    row[idx] = c_type(row[idx])
+                print(row)
+                self.data.append(row)
+
+    def save_to_csv(self, filename):
+        with open(filename, 'wt', encoding='utf-8') as f:
+            f.write(','.join(self.cols))
+            f.write('\n')
+            for row in self.data:
+                f.write(','.join(row))
+
+    def lookup_key(self, key):
+        if key in self.cols:
+            return self.cols.index(key)
+        else:
+            raise ValueError('Invalid key')
+
+    def lookup_row(self, key, value):
+        col_idx = self.lookup_key(key)
+        value = self.column_types[col_idx](value)  # lets do string comparisons for now
+        for row_idx, row in enumerate(self.data):
+            if row[col_idx] == value:
+                return row_idx
+        return None
+        # raise ValueError('Could not find row')
+
+    def get_col(self, key):
+        result = []
+        col_idx = self.lookup_key(key)
+        for row in self.data:
+            result.append(row[col_idx])
+        return result
+
+    def get_item(self, key, index):
+        return self.data[index][self.lookup_key(key)]
+
+    def print_all(self):
+        print(tabulate(self.data, headers=self.cols))
+
+    def print_selection(self, keys):
+        col_idx = [self.lookup_key(key) for key in keys]
+        data = []
+        for row in self.data:
+            data.append(row[i] for i in col_idx)
+        header = [self.cols[i] for i in col_idx]
+        print(tabulate(data, headers=header))
+
+    def sort_rows(self, key):
+        col = self.get_col(key)
+        print(col)
+        sort_index = [i[0] for i in sorted(enumerate(col), key=lambda x:x[1], reverse=True)]
+        self.data = [self.data[i] for i in sort_index]
+        # and re index?
+        icol = self.lookup_key('index')
+        print(icol)
+        if icol is not None:
+            for ind, row in enumerate(self.data):
+                row[icol] = ind
+
+    def add_row(self, entry: dict, matchkey: str):
+        # check if row is already in database
+        append_row = False
+        row_idx = self.lookup_row(matchkey, entry[matchkey])
+        if row_idx is not None:
+            # we have a known thing
+            row = self.data[row_idx]
+        else:
+            row = [''] * len(self.cols)
+            append_row = True
+        for key, value in entry.items():
+            col_idx = self.lookup_key(key)
+            row[col_idx] = self.column_types[col_idx](value)
+        if append_row:
+            self.data.append(row)
 
 
 def convert_slurm_time(time_str: str):
@@ -22,7 +112,7 @@ class SlurmManager:
         self.database_path = os.path.join(
             self.database_folder, self.database_file)
 
-        self.columns = ["JobID", "JobName", "WorkDir", "Partition",
+        self.columns = ["index", "JobID", "JobName", "WorkDir", "Partition",
                         "Output", "State", "Elapsed", "Start", "End"]
         self.column_types = [int, str, str, str, str, str, str, str, str]
         self.database = None
@@ -162,4 +252,28 @@ if __name__ == "__main__":
     #     entry = entry.split('|')
     #     print(entry)
 
-    pass
+    cols = ["index", "JobID", "JobName", "WorkDir", "Partition",
+            "Output", "State", "Elapsed", "Start", "End"]
+    types = [int, int, str, str, str, str, str, str, str, str]
+    db = Database(columns=cols, column_types=types)
+
+    db.load_from_csv('test.csv')
+    # db.save_to_csv('test_out.csv')
+
+    print(db.lookup_key('JobID'))
+    print(db.lookup_key('JobName'))
+    print(db.lookup_row('JobID', 2065857))
+    print(db.get_item('JobID', 1))
+
+    db.print_selection(['index', 'JobID', 'Start'])
+
+    db.print_all()
+
+    e = {
+        "JobID": 12312345,
+        "JobName": "lalal"
+    }
+    db.add_row(e, "JobID")
+    db.print_all()
+    db.sort_rows("JobID")
+    db.print_all()
